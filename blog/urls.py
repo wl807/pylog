d4f64b466f93949f407ef0a4a6321777b2ce1544
wl807 <mswl0807@gmail.com>
2023-12-07 00:18:11 +0900

    extends 반영

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list ),
+    path('', views.post_list , name="post_list"),
     # <> 으로 동적으로 값을 받고, 해당하는 view 의 인수로 전달
     path('<int:post_id>/', views.post_detail, name="post_detail" ), 
     path('add/', views.post_add, name="post_add" ), 
